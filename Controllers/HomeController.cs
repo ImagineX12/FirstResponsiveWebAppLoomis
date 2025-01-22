df0ca519484f6641b02966584fe2ba7cfbe442d6
ImagineX12 <86370767+ImagineX12@users.noreply.github.com>
2025-01-21 20:24:58 -0600

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using FirstResponsiveWebAppLoomis.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FirstResponsiveWebAppLoomis.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Handels Get Requests
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Output = "";
+            return View();
+        }
+
+        //Handels Post Requests
+        [HttpPost]
+        public IActionResult Index(BirthdayModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Output = model.Output();
+            }
+            else
+            {
+                ViewBag.Output = 0;
+            }
+            return View(model);
+        }
+    }
+}
